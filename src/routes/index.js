6586b2ca290d142b87c7e8a290b9574561a584c4
AMANI Eric <amaniericus@gmail.com>
2021-09-21 16:12:16 +0200

    ft(404):add 4004 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import { RouteNames } from "../config/RouteNames";
+import NotFound from "../pages/404";
 import ForgotPassword from "../pages/auth/ForgotPassword";
 import Login from "../pages/auth/Login";
 
@@ -10,6 +11,7 @@ return (
         <Switch>
         <Route exact path={RouteNames.login} component={Login} />
         <Route  exact path={RouteNames.forgot_password} component={ForgotPassword}/>
+        <Route  exact component={NotFound} />
         </Switch>
     </Router>
 );
