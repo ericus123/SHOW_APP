11852296c72605904a00dbd914a40ea07cf799e9
AMANI Eric <amaniericus@gmail.com>
2021-09-27 05:38:55 -0400

    ft(notification):add notification panel

---
+++ src/pages/dashboard/EventDetails.js
@@ -0,0 +1,26 @@
+import { Col, Row } from "antd";
+import { Content} from "antd/lib/layout/layout";
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+const EventDetails = () => {
+
+    return (
+
+        <DashboardLayout>
+        <Content>
+        
+        <Row>
+
+<Col className="event-details-col1">
+
+</Col>
+<Col clas>
+</Col>
+        </Row>
+        </Content>
+        </DashboardLayout>
+       
+    );
+};
+
+export default EventDetails;
